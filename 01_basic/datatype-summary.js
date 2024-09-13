6c75e352657269149a2ac16f37b70233c79973c7
Kuhali Paul <116445608+Kuhalipaul@users.noreply.github.com>
2024-09-13 18:51:27 +0000

    datatype-summary

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,37 @@
+// PEIMITIVE datatype[copy karke usmai changes hota hai]
+
+//  -7category: String, Number, Boolean, null, undefind, symbol, BigInt
+//********************************************************************/
+// JavaScript is dynamically typed. 
+// This means that the type of a variable is determined at runtime, not at compile time.
+const scroe=100;
+const scoreValue=100.3
+
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=symbol("123")
+const anotherId= Symbol('123')
+
+// console.log(id=== anotherId);
+
+const bigNumber=3547893458n
+
+//********************************************************************************/
+//Reference type or (Non Primitive type)
+
+//Array, Objects, Functions
+
+const heros= ["shaktiman", "naagraj", "doga"];
+let myObj={
+    name:"kuhali",
+    age:25,
+}
+
+const myFunction=function(){
+console.log("hello world");
+}
+
+console.log(typeof bigNumber);
